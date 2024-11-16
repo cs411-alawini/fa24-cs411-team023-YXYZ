792ec14a7d82f415b285a30d5fd823181b4ef9c5
zihan939 <zli2437@wisc.edu>
2024-11-16 00:36:01 -0600

    sign up & login finished

---
+++ ecovista/backend/signUp.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI, HTTPException, status
+from pydantic import BaseModel
+import pymysql
+import os
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+app = FastAPI()
+
+# Add CORS middleware to allow the frontend to communicate with the backend
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# MySQL Database Connection Configuration
+db_config = {
+    'host': os.getenv('DB_HOST'),  
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'database': os.getenv('DB_NAME'),
+}
+
+# model for request body
+class RegisterRequest(BaseModel):
+    email: str
+    nickname: str
+    county_code: int
+    password: str
+
+class Login(BaseModel):
+    email: str
+    password: str
+
+# SignUp Endpoint
+@app.post('/register', status_code=status.HTTP_201_CREATED)
+async def register_user(request: RegisterRequest):
+    # print(request)
+    connection = None
+    cursor = None
+    try:
+        connection = pymysql.connect(
+            host='34.173.41.58',
+            user='test',
+            password='yxyz',
+            database='EcoVista'
+        )
+        print("成功连接到数据库")
+        with connection.cursor() as cursor:
+            # Query to fetch the first 10 rows from the Location table
+            insert_user_query = 'INSERT INTO UserProfile (email, username, county_code, password) VALUES (%s, %s, %s, %s)'
+            cursor.execute(insert_user_query, (request.email, request.nickname, request.county_code, request.password))
+            connection.commit()
+
+            return {'success': True, 'message': 'Registration successful.'}
+
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to MySQL Database: {e}")
+    finally:
+        if 'connection' in locals() and connection.open:
+            connection.close()
+
+
+
+# Login Endpoint
+@app.post('/login', status_code=status.HTTP_201_CREATED)
+async def register_user(request: Login):
+    print(request)
+    connection = None
+    cursor = None
+    try:
+        connection = pymysql.connect(
+            host='34.173.41.58',
+            user='test',
+            password='yxyz',
+            database='EcoVista'
+        )
+        print("成功连接到数据库")
+        cursor = connection.cursor(pymysql.cursors.DictCursor)
+        with cursor:
+            check_user_query = 'SELECT * FROM UserProfile WHERE email = %s AND password = %s'
+            cursor.execute(check_user_query, (request.email, request.password))
+            user = cursor.fetchone()
+            print('user')
+            print(user)
+
+            if user is None:
+                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid email or password.')
+
+            return {
+                'success': True,
+                'nickname': user['username'],
+                'user_id': user['user_id'],
+                'user_county_code':user['county_code'],
+                'message': 'Login successful.'
+            }
+
+    except pymysql.MySQLError as e:
+        print(f"Error connecting to MySQL Database: {e}")
+    finally:
+        if 'connection' in locals() and connection.open:
+            connection.close()
